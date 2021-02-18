95094024f21db98e5f201b76ac6c258236679900
rbiiiii <robinfoguenne@territoires-memoire.be>
2021-02-18 15:17:52 +0100

    update video data for multinlingual

--- src/main.js
+++ src/main.js
@@ -8,47 +8,7 @@ const app = new App({
                 title: "fr"
             },
             {
-                source: 'Visuelles_Couleursseparees_fr',
-                title: "fr"
-            },
-            {
-                source: 'Visuelles_Hallucinationsvisuelles_fr',
-                title: "fr"
-            },
-            {
-                source: 'Visuelles_Imposssible typography_fr',
-                title: "fr"
-            },
-            {
-                source: 'Visuelles_Masquespleins_fr',
-                title: "fr"
-            },
-            {
-                source: 'Visuelles_noirblanccouleurs_fr',
-                title: "fr"
-            },
-            {
                 source: 'Visuelle_DoubleAxis_nl',
-                title: "nl"
-            },
-            {
-                source: 'Visuelles_Couleursseparees_nl',
-                title: "nl"
-            },
-            {
-                source: 'Visuelles_Hallucinationsvisuelles_nl',
-                title: "nl"
-            },
-            {
-                source: 'Visuelles_Imposssible typography_nl',
-                title: "nl"
-            },
-            {
-                source: 'Visuelles_Masquespleins_nl',
-                title: "nl"
-            },
-            {
-                source: 'Visuelles_noirblanccouleurs_nl',
                 title: "nl"
             },
             {
@@ -56,49 +16,89 @@ const app = new App({
                 title: "de"
             },
             {
-                source: 'Visuelles_Couleursseparees_de',
-                title: "de"
-            },
-            {
-                source: 'Visuelles_Hallucinationsvisuelles_de',
-                title: "de"
-            },
-            {
-                source: 'Visuelles_Imposssible typography_de',
-                title: "de"
-            },
-            {
-                source: 'Visuelles_Masquespleins_de',
-                title: "de"
-            },
-            {
-                source: 'Visuelles_noirblanccouleurs_de',
-                title: "de"
-            },
-            {
                 source: 'Visuelle_DoubleAxis_en',
                 title: "en"
+            },
+            {
+                source: 'Visuelles_Couleursseparees_fr',
+                title: "fr"
+            },
+            {
+                source: 'Visuelles_Couleursseparees_nl',
+                title: "nl"
+            },
+            {
+                source: 'Visuelles_Couleursseparees_de',
+                title: "de"
             },
             {
                 source: 'Visuelles_Couleursseparees_en',
                 title: "en"
             },
             {
+                source: 'Visuelles_Hallucinationsvisuelles_fr',
+                title: "fr"
+            },
+            {
+                source: 'Visuelles_Hallucinationsvisuelles_nl',
+                title: "nl"
+            },
+            {
+                source: 'Visuelles_Hallucinationsvisuelles_de',
+                title: "de"
+            },
+            {
                 source: 'Visuelles_Hallucinationsvisuelles_en',
                 title: "en"
+            },
+            {
+                source: 'Visuelles_Imposssible typography_fr',
+                title: "fr"
+            },
+            {
+                source: 'Visuelles_Imposssible typography_nl',
+                title: "nl"
+            },
+            {
+                source: 'Visuelles_Imposssible typography_de',
+                title: "de"
             },
             {
                 source: 'Visuelles_Imposssible typography_en',
                 title: "en"
             },
             {
+                source: 'Visuelles_Masquespleins_fr',
+                title: "fr"
+            },
+            {
+                source: 'Visuelles_Masquespleins_nl',
+                title: "nl"
+            },
+            {
+                source: 'Visuelles_Masquespleins_de',
+                title: "de"
+            },
+            {
                 source: 'Visuelles_Masquespleins_en',
                 title: "en"
             },
             {
+                source: 'Visuelles_noirblanccouleurs_fr',
+                title: "fr"
+            },
+            {
+                source: 'Visuelles_noirblanccouleurs_nl',
+                title: "nl"
+            },
+            {
+                source: 'Visuelles_noirblanccouleurs_de',
+                title: "de"
+            },
+            {
                 source: 'Visuelles_noirblanccouleurs_en',
                 title: "en"
-            }
+            },
         ]
     }
 });
