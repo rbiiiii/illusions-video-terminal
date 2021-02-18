8c6f614b11c2ceae35816b7f6b12e23f098b56f0
rbiiiii <robinfoguenne@territoires-memoire.be>
2021-02-18 15:24:47 +0100

    update video data for multinlingual

--- src/main.js
+++ src/main.js
@@ -5,27 +5,99 @@ const app = new App({
     props: {
         videosList: [{
                 source: 'Cognitive_Flashs_fr',
-                title: "FR"
+                title: "fr"
+            },
+            {
+                source: 'Cognitive_Flashs_nl',
+                title: "nl"
+            },
+            {
+                source: 'Cognitive_Flashs_de',
+                title: "de"
+            },
+            {
+                source: 'Cognitive_Flashs_en',
+                title: "en"
             },
             {
                 source: 'Cognitive_gorille_fr',
-                title: "FR"
+                title: "fr"
+            },
+            {
+                source: 'Cognitive_gorille_nl',
+                title: "nl"
+            },
+            {
+                source: 'Cognitive_gorille_de',
+                title: "de"
+            },
+            {
+                source: 'Cognitive_gorille_en',
+                title: "en"
             },
             {
                 source: 'Cognitive_Koulechov_fr',
-                title: "FR"
+                title: "fr"
+            },
+            {
+                source: 'Cognitive_Koulechov_nl',
+                title: "nl"
+            },
+            {
+                source: 'Cognitive_Koulechov_de',
+                title: "de"
+            },
+            {
+                source: 'Cognitive_Koulechov_en',
+                title: "en"
             },
             {
                 source: 'Cognitive_Train_fr',
-                title: "FR"
+                title: "fr"
+            },
+            {
+                source: 'Cognitive_Train_nl',
+                title: "nl"
+            },
+            {
+                source: 'Cognitive_Train_de',
+                title: "de"
+            },
+            {
+                source: 'Cognitive_Train_en',
+                title: "en"
             },
             {
                 source: 'Cognitives_Ballerine_fr',
-                title: "FR"
+                title: "fr"
+            },
+            {
+                source: 'Cognitives_Ballerine_nl',
+                title: "nl"
+            },
+            {
+                source: 'Cognitives_Ballerine_de',
+                title: "de"
+            },
+            {
+                source: 'Cognitives_Ballerine_en',
+                title: "en"
             },
             {
                 source: 'Cognitives_Pointscouleur_fr',
-                title: "FR"
+                title: "fr"
+            },
+            {
+                source: 'Cognitives_Pointscouleur_nl',
+                title: "nl"
+            },
+            {
+                source: 'Cognitives_Pointscouleur_de',
+                title: "de"
+            },
+            {
+                source: 'Cognitives_Pointscouleur_en',
+                title: "en"
             }
         ]
     }
